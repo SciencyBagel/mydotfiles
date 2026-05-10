ff5d7098ef7190ef1fbfea6e5486791af5880a4e
Jahir <jfm.post@icloud.com>
2026-05-10 14:51:30 -0500

    outline wider by default

---
+++ home/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/trouble.nvim",
+  opts = {
+    modes = {
+      symbols = {
+        win = {
+          -- Default is 30
+          width = 50,
+        }
+      }
+    }
+  }
+}
