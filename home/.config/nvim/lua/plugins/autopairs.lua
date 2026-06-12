bcda8dcf304ff1852d1f23375b636718bf0e7420
Jahir F. Montes <jahir.montes@is.l3t.com>
2026-06-12 14:39:13 -0500

    delete minipairs

--- home/.config/nvim/lua/plugins/autopairs.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "windwp/nvim-autopairs",
-
-  -- stop auto-parens on completion
-  config = function(_, opts)
-    local autopairs = require("nvim-autopairs")
-    autopairs.setup(opts)
-  end
-}
