aa4b81e90f59b26558596af08d27e697bb911aca
Jahir Montes <montesjf@nomad.u.l3>
2026-07-16 18:06:31 -0500

    feat(nvim): bind table of contents gen to <leader>mt

--- home/.config/nvim/lua/config/keymaps.lua
+++ home/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Bind Table of Contents generation to <leader>mt
+vim.keymap.set("n", "<leader>mt", function()
+  -- If you use mzlogin/vim-markdown-toc
+  vim.cmd("Mtoc insert")
+  -- Alternatively, if you use hedyhli/markdown-toc.nvim, use: vim.cmd("Mtoc insert")
+end, { desc = "Markdown Generate TOC" })
